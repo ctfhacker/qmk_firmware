a0717d2d8fe38d588d1c31928fc5733705d49bbc
ctfhacker <cld251@gmail.com>
2023-12-05 11:54:40 -0600

    Updates

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 qmk compile -kb keychron/q1v2/ansi -km ctfhacker
-qmk flash -kb keychron/q1v2/ansi -km ctfhacker -bl dfu
+qmk flash -kb keychron/q1v2/ansi -km ctfhacker
+qmk flash -kb keychron/q1v2/ansi -km ctfhacker
+qmk flash -kb keychron/q1v2/ansi -km ctfhacker
+qmk flash -kb keychron/q1v2/ansi -km ctfhacker
